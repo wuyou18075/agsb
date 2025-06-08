3edab742f4000899189c31e43923f45cde19e912
wuyou18075 <wuyou18075@gmail.com>
2025-06-08 21:41:32 +0800

    Update upload-agsb-v2-cj.py
    USER_NAME=
    UUID=
    TOKEN=
    DOMAIN=
    启动可添加上边的变量值

--- upload-agsb-v2-cj.py
+++ upload-agsb-v2-cj.py
@@ -30,13 +30,14 @@ LOG_FILE = INSTALL_DIR / "argo.log"
 DEBUG_LOG = INSTALL_DIR / "python_debug.log"
 CUSTOM_DOMAIN_FILE = INSTALL_DIR / "custom_domain.txt" # 存储最终使用的域名
 
-# ====== 全局可配置参数（可直接在此处修改） ======
-USER_NAME = "kkddytd"         # 用户名
-UUID = "a91b59b6-ade4-497d-b4e9-88d184c48048"                     # UUID，留空则自动生成
-PORT = 49999                   # Vmess端口，留空或0则自动生成
-DOMAIN = "free.testlrj.ddns-ip.net"                   # 域名，留空则自动获取
-CF_TOKEN = "eyJhIjoiODBmMjY5ZmQ1N2QzNzNiMmMzZTBkODc4ODg1NWM5MzIiLCJ0IjoiZmVhMzBmODUtOGY5OC00ZTVmLTkyZTktMmU2OTk2M2E1YzUyIiwicyI6Ik4yTmlZemxpTlRjdE5UVm1PQzAwTjJZekxXRmpORGt0TVdVNE5HUmtORGN3TldObSJ9"                 # Cloudflare Token，留空则用Quick Tunnel
+# ====== 全局可配置参数（可通过环境变量覆盖） ======
+USER_NAME = os.getenv("USER_NAME", "521")  # 用户名，默认值为 "521"
+UUID = os.getenv("UUID", "")  # UUID，默认值为指定的UUID
+PORT = int(os.getenv("PORT", ""))  # Vmess端口，留空或0则自动生成
+DOMAIN = os.getenv("DOMAIN", "")  # 域名，留空则自动获取
+CF_TOKEN = os.getenv("TOKEN", "")  # Cloudflare Token，默认值为空（使用Quick Tunnel）
 # =========================================
+
 
 # 添加命令行参数解析
 def parse_args():
